f89215f132801b71d3ea3260885df21eb3896e87
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-14 17:29:37 +0200

    wip

--- src/processor.rs
+++ src/processor.rs
@@ -1,9 +1,30 @@
-use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey};
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::AccountInfo, entrypoint::ProgramResult, program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub enum TransferInstruction {
+    Initialize(),
+    DepositSol(u64),
+    WithdrawSol(),
+}
 
 pub fn process_instruction(
     _program_id: &Pubkey,
     _accounts: &[AccountInfo],
-    _instruction_data: &[u8],
+    instruction_data: &[u8],
 ) -> ProgramResult {
-    Ok(())
+    let instruction = TransferInstruction::try_from_slice(instruction_data);
+
+    match instruction {
+        Ok(TransferInstruction::Initialize()) => Ok(()),
+
+        Ok(TransferInstruction::DepositSol(args)) => Ok(()),
+
+        Ok(TransferInstruction::WithdrawSol()) => Ok(()),
+
+        Err(err) => Err(ProgramError::InvalidInstructionData),
+    }
 }
