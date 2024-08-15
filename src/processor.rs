518311722841d8cfd4e9f10d680aff88a6d6e669
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-15 22:13:43 +0200

    simplified process instruction

--- src/processor.rs
+++ src/processor.rs
@@ -1,9 +1,5 @@
 use borsh::BorshDeserialize;
-use solana_program::{
-    account_info::AccountInfo, entrypoint::ProgramResult, msg, program_error::ProgramError,
-    pubkey::Pubkey,
-};
-use std::io::Error;
+use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey};
 
 use crate::{
     instructions::{deposit, withdraw},
@@ -15,18 +11,11 @@ pub fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    let instruction: Result<SolAccountInstruction, Error> =
-        SolAccountInstruction::try_from_slice(instruction_data);
+    let instruction: SolAccountInstruction =
+        SolAccountInstruction::try_from_slice(instruction_data)?;
 
     match instruction {
-        Ok(SolAccountInstruction::DepositSol(amount)) => deposit(program_id, accounts, amount),
-
-        Ok(SolAccountInstruction::WithdrawSol()) => withdraw(program_id, accounts),
-
-        Err(err) => {
-            msg!("An error occured: {}", err);
-
-            Err(ProgramError::InvalidInstructionData)
-        }
+        SolAccountInstruction::DepositSol(amount) => deposit(program_id, accounts, amount),
+        SolAccountInstruction::WithdrawSol() => withdraw(program_id, accounts),
     }
 }
