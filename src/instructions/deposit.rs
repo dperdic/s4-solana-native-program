7fab493216b5e865140d19e54a2a583a6231bc02
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-17 00:26:56 +0200

    simplified deposit and withdraw functions

--- src/instructions/deposit.rs
+++ src/instructions/deposit.rs
@@ -2,18 +2,16 @@ use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
     instruction::Instruction,
-    msg,
-    program::invoke_signed,
+    program::{invoke, invoke_signed},
     program_error::ProgramError,
-    program_pack::Pack,
     pubkey::Pubkey,
     rent::Rent,
     system_instruction::{create_account, transfer},
     sysvar::Sysvar,
 };
-use std::{cell::RefMut, slice::Iter};
+use std::slice::Iter;
 
-use crate::state::{SolAccount, PDA_SOL_ACCOUNT_SEED};
+use crate::state::PDA_SOL_ACCOUNT_SEED;
 
 pub fn deposit(program_id: &Pubkey, accounts: &[AccountInfo], amount: u64) -> ProgramResult {
     let accounts_iter: &mut Iter<AccountInfo> = &mut accounts.iter();
@@ -30,46 +28,25 @@ pub fn deposit(program_id: &Pubkey, accounts: &[AccountInfo], amount: u64) -> Pr
         return Err(ProgramError::InsufficientFunds);
     }
 
+    // Derive PDA
     let (pda, bump_seed) = Pubkey::find_program_address(
         &[PDA_SOL_ACCOUNT_SEED, user_account.key.as_ref()],
         program_id,
     );
 
-    if sol_account.key != &pda {
-        return Err(ProgramError::InvalidAccountData);
+    if pda != *sol_account.key {
+        return Err(ProgramError::InvalidArgument);
     }
 
-    let mut sol_data: SolAccount;
+    // Check if PDA exists and create it if it doesn't
+    if sol_account.owner == system_program.key {
+        let rent: u64 = Rent::get()?.minimum_balance(0);
 
-    {
-        let sol_account_data: &mut RefMut<&mut [u8]> = &mut sol_account.try_borrow_mut_data()?;
-
-        sol_data = match SolAccount::unpack(&sol_account_data) {
-            Ok(data) => data,
-
-            Err(_) => SolAccount {
-                is_initialized: false,
-                owner: *user_account.key,
-                balance: 0,
-            },
-        };
-    }
-
-    // Initialize PDA if it doesn't exist
-    if !sol_data.is_initialized {
-        let rent: Rent = Rent::get()?;
-        let required_lamports: u64 = rent.minimum_balance(SolAccount::LEN);
-
-        let create_pda_account_ix: Instruction = create_account(
-            user_account.key,
-            sol_account.key,
-            required_lamports,
-            SolAccount::LEN as u64,
-            program_id,
-        );
+        let create_pda_instruction: Instruction =
+            create_account(user_account.key, sol_account.key, rent, 0, program_id);
 
         invoke_signed(
-            &create_pda_account_ix,
+            &create_pda_instruction,
             &[
                 user_account.clone(),
                 sol_account.clone(),
@@ -81,49 +58,19 @@ pub fn deposit(program_id: &Pubkey, accounts: &[AccountInfo], amount: u64) -> Pr
                 &[bump_seed],
             ]],
         )?;
-
-        sol_data = SolAccount {
-            is_initialized: true,
-            owner: *user_account.key,
-            balance: 0,
-        };
-
-        let sol_account_data: &mut RefMut<&mut [u8]> = &mut sol_account.try_borrow_mut_data()?;
-
-        SolAccount::pack(sol_data, sol_account_data)?;
-
-        msg!("Created and initialized PDA");
     }
 
-    // Transfer lamports from user to PDA
-    let transfer_ix: Instruction = transfer(user_account.key, sol_account.key, amount);
+    // Transfer lamports from user's account to PDA
+    let transfer_instruction: Instruction = transfer(user_account.key, sol_account.key, amount);
 
-    invoke_signed(
-        &transfer_ix,
+    invoke(
+        &transfer_instruction,
         &[
             user_account.clone(),
             sol_account.clone(),
             system_program.clone(),
         ],
-        &[&[
-            PDA_SOL_ACCOUNT_SEED,
-            user_account.key.as_ref(),
-            &[bump_seed],
-        ]],
     )?;
-
-    // Borrow and unpack PDA account data
-    let sol_account_data: &mut RefMut<&mut [u8]> = &mut sol_account.try_borrow_mut_data()?;
-
-    sol_data = SolAccount::unpack(&sol_account_data)?;
-
-    // Update PDA balance
-    sol_data.balance = sol_data
-        .balance
-        .checked_add(amount)
-        .ok_or(ProgramError::ArithmeticOverflow)?;
-
-    SolAccount::pack(sol_data, sol_account_data)?;
 
     Ok(())
 }
--- src/instructions/withdraw.rs
+++ src/instructions/withdraw.rs
@@ -1,84 +1,48 @@
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
-    instruction::Instruction,
-    program::invoke_signed,
     program_error::ProgramError,
-    program_pack::Pack,
     pubkey::Pubkey,
-    system_instruction::transfer,
 };
-use std::{cell::RefMut, slice::Iter};
+use std::slice::Iter;
 
-use crate::state::{SolAccount, PDA_SOL_ACCOUNT_SEED};
+use crate::state::PDA_SOL_ACCOUNT_SEED;
 
 pub fn withdraw(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
     let accounts_iter: &mut Iter<AccountInfo> = &mut accounts.iter();
 
     let user_account: &AccountInfo = next_account_info(accounts_iter)?;
     let sol_account: &AccountInfo = next_account_info(accounts_iter)?;
-    let system_program: &AccountInfo = next_account_info(accounts_iter)?;
+    let _system_program: &AccountInfo = next_account_info(accounts_iter)?;
 
-    // Ensure the user is a signer
     if !user_account.is_signer {
         return Err(ProgramError::MissingRequiredSignature);
     }
 
-    let (pda, bump_seed) = Pubkey::find_program_address(
+    // Derive PDA
+    let (pda, _bump_seed) = Pubkey::find_program_address(
         &[PDA_SOL_ACCOUNT_SEED, user_account.key.as_ref()],
         program_id,
     );
 
-    if sol_account.key != &pda {
+    if pda != *sol_account.key {
         return Err(ProgramError::InvalidArgument);
     }
 
-    // Ensure PDA exists
-    if sol_account.data_is_empty() {
-        return Err(ProgramError::UninitializedAccount);
-    }
-
-    // Borrow and unpack PDA account data
-    let sol_account_data: &mut RefMut<&mut [u8]> = &mut sol_account.try_borrow_mut_data()?;
-    let mut sol_data: SolAccount = SolAccount::unpack(sol_account_data)?;
-
-    if sol_data.owner != *user_account.key {
+    // Check if PDA is owned by the program
+    if sol_account.owner != program_id {
         return Err(ProgramError::IllegalOwner);
     }
 
-    let amount: u64 = sol_data
-        .balance
+    let sol_account_balance = sol_account.lamports();
+
+    let amount = sol_account_balance
         .checked_div(10)
         .ok_or(ProgramError::ArithmeticOverflow)?;
 
-    if amount > sol_account.lamports() {
-        return Err(ProgramError::InsufficientFunds);
-    }
-
-    // Transfer lamports from PDA to user
-    let transfer_ix: Instruction = transfer(sol_account.key, user_account.key, amount);
-
-    invoke_signed(
-        &transfer_ix,
-        &[
-            sol_account.clone(),
-            user_account.clone(),
-            system_program.clone(),
-        ],
-        &[&[
-            PDA_SOL_ACCOUNT_SEED,
-            user_account.key.as_ref(),
-            &[bump_seed],
-        ]],
-    )?;
-
-    // Update PDA data
-    sol_data.balance = sol_data
-        .balance
-        .checked_sub(amount)
-        .ok_or(ProgramError::ArithmeticOverflow)?;
-
-    SolAccount::pack(sol_data, sol_account_data)?;
+    // Transfer 10% of PDA balance to user
+    **sol_account.try_borrow_mut_lamports()? -= amount;
+    **user_account.try_borrow_mut_lamports()? += amount;
 
     Ok(())
 }
