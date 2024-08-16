668d5c8a7679522e48f15ffd53632f679532ee09
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-16 10:45:49 +0200

    update packages

--- tests/test.ts
+++ tests/test.ts
@@ -1,5 +1,46 @@
-describe("something", () => {
-  it("should run", () => {
-    console.log("log something");
-  });
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  RpcResponseAndContext,
+  SignatureResult,
+  TransactionInstruction,
+} from "@solana/web3.js";
+
+const programId = new PublicKey("SsLs4JcUzhftkoMSuTSsdNJazY6X8uV3KmEV56tmfpw");
+
+const userKeypair = Keypair.generate();
+
+const connection = new Connection("http://localhost:8899", {
+  commitment: "confirmed",
 });
+
+describe("deposit and withdraw tests", () => {
+  before(async () => {});
+
+  after(async () => {});
+
+  it("should deposit sol", async () => {
+    new TransactionInstruction({
+      programId: programId,
+      keys: [],
+    });
+  });
+
+  it("should withdraw sol", async () => {});
+});
+
+const confirmTransaction = async (
+  tx: string
+): Promise<RpcResponseAndContext<SignatureResult>> => {
+  const bh = await connection.getLatestBlockhash();
+
+  return await connection.confirmTransaction(
+    {
+      signature: tx,
+      blockhash: bh.blockhash,
+      lastValidBlockHeight: bh.lastValidBlockHeight,
+    },
+    "confirmed"
+  );
+};
